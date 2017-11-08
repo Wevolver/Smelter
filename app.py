840c6395b3868c8ae6b2fdf3846ad715a0a6857f
Rodrigo <rodrigo.trespalacios@gmail.com>
2017-11-08 11:30:57 +0100

    typo

--- app.py
+++ app.py
@@ -41,7 +41,7 @@ def read_step_file(filename):
     return aResShape
 
 def import_as_one_shape(file):
-    shp = read_step_file(name)
+    shp = read_step_file(file)
     tess = Tesselator(shp)
     tess.Compute()
     threejsString = tess.ExportShapeToThreejsJSONString('someid')
